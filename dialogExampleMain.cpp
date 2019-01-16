d5aff8215298b933beda31d505cd9e57ae585816
Jasper Oosterbroek <31768732+JasperOosterbroek@users.noreply.github.com>
2019-01-16 23:13:36 +0100

    Update dialogExampleMain.cpp

--- dialogExampleMain.cpp
+++ dialogExampleMain.cpp
@@ -21,13 +21,10 @@ int main(int argc, char *argv[]) {
 				window.close();
 			}
 		}
-		std::cout << "start zooi" << '\n';
 		std::string str = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 		str += std::to_string(count);
-		std::cout << "outside game: " << '\n';
 		diaBox.print(str);
 		count++;
-		std::cout << "eind zooi" << '\n';
 
 		window.display();
 
