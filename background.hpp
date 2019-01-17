d9c185e052ad5476ffe14eb47f19f1f4da278412
Joepieler <be.campagne@live.nl>
2019-01-17 10:27:42 +0100

    Add doxygen

--- background.hpp
+++ background.hpp
@@ -1,19 +1,34 @@
 #pragma once
-
 #include <SFML/Graphics.hpp>
 #include <utility> 
 #include <string>
 #include <vector>
 
+
+///@file
+
+///\brief
+/// A class that create and set the background.
 class BackGround {
 private:
 	sf::Sprite sprite;
 	sf::Texture texture;
 	std::vector<std::pair<std::string, std::string>> backGrounds;
 public:
-	void add(const std::string &name, std::string &fileName);
-	std::string getPath(const std::string & backGroundName);
-	void SetBackGround(const std::string & backGroundName);
-	void draw(sf::RenderWindow &window);
+	///\brief
+	///Add a  std::pair of image name and fileme to the vector.
+	void add(const std::string &, const std::string &);
+	
+	///\brief
+	///Gets the path of the image that you are searching for.
+	std::string getPath(const std::string &);
+	
+	///\brief
+	/// Sets a backgroud.
+	void SetBackGround(const std::string &);
+	
+	///\brief
+	///Draws the backgroud.
+	void draw(sf::RenderWindow &);
 };
 
