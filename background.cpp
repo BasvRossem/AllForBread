17cfa01714a7e61a9863dbaab9030615bb37233b
Joepieler <be.campagne@live.nl>
2019-01-17 16:10:53 +0100

    add scaling to the background

--- background.cpp
+++ background.cpp
@@ -13,13 +13,15 @@ std::string BackGround::getPath(const std::string & backGroundName) {
 	}
 	return 0;
 }
-void BackGround::SetBackGround(const std::string & backGroundName){
-	if (texture.loadFromFile(getPath(backGroundName))) {
-		sprite.setTexture(texture);
-		sprite.setPosition(0,0);
+void BackGround::SetBackGround(const std::string & backGroundName, sf::RenderWindow & window){
+	if (backgroundTexture.loadFromFile(getPath(backGroundName))){
+		sf::Vector2f size = sf::Vector2f{ backgroundTexture.getSize() };
+		backgroundSprite.setTexture(backgroundTexture);
+		backgroundSprite.setPosition(0, 0);
+		backgroundSprite.scale(sf::Vector2f{ window.getSize().x / size.x, window.getSize().y / size.y });
 	}
 }
 
 void BackGround::draw(sf::RenderWindow &window) {
-	window.draw(sprite);
+	window.draw(backgroundSprite);
 }
