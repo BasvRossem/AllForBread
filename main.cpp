e6c84ba18d0e2d8969395cf40b0749591b5981c7
Bas van Rossem <bas.vanrossem@student.hu.nl>
2019-01-17 20:32:06 +0100

    Create main.cpp
    Added a main

---
+++ main.cpp
@@ -0,0 +1,56 @@
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#include <iterator>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include "Character/Party.hpp"
+#include "states/Combat.hpp"
+#include "Character/Monster.hpp"
+
+int main(){
+
+	//-Monster Disco Party Vector
+	//==========================
+	std::vector<std::shared_ptr<Monster>> MonsterClassVector;
+
+	//for (unsigned int i = 0; i < 20; i++) {
+	//	MonsterClassVector.push_back(std::make_shared<Monster>("Testsubject Zero", "Assets/GorillaIdle.png"));
+	//}
+	//==========================
+
+
+	//test SFML
+	sf::RenderWindow window(sf::VideoMode(1920, 1080), "The Holy Bread of Takatiki", sf::Style::Fullscreen);
+	window.setFramerateLimit(0);
+
+	PlayerCharacter testCharacter = PlayerCharacter("Anubis, the distructor of hopes and dreams","Assets/AnubisIdle.png");
+	Character testMonster= Character("Rupert, the Poopert Robot", "Assets/RobotIdle.png", 12);
+	testMonster.makeMonster();
+
+	std::vector<std::shared_ptr<PlayerCharacter>> heroVector = {std::make_shared<PlayerCharacter>(testCharacter)};
+
+	std::vector< std::shared_ptr<Character>> monsterVector = { std::make_shared <Character>(testMonster) };
+	CharacterContainer< std::shared_ptr<Character >> monsters = (monsterVector);
+	Party heroParty(heroVector);
+
+	std::string backgroundImage = "Assets/background680.png";
+	Combat testCombat(window, heroParty, monsters, backgroundImage);
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+		window.clear();
+		
+		testCombat.update();
+
+		window.display();
+	} //END test SFML
+	return 0;
+}
