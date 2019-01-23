5a11940ac16491d5b8d77fd3540e49f14a9ab191
Jens Bouman <jens.bouman@student.hu.nl>
2019-01-23 10:01:28 +0100

    Added item name and description

--- Items/Item.cpp
+++ Items/Item.cpp
@@ -1,11 +1,24 @@
 #include "Item.hpp"
 
+Item::Item() :
+	name(std::string()),
+	description(std::string())
+{}
+
 int Item::getWeight() {
 	return weight;
 }
 
 int Item::getBaseValue() {
 	return baseValue;
+}
+
+std::string Item::getName() {
+	return name;
+}
+
+std::string Item::getDescription() {
+	return description;
 }
 
 void Item::setWeight(const int & newWeight) {
@@ -18,4 +31,12 @@ void Item::setBaseValue(const int & newBaseValue) {
 	if (newBaseValue >= 0) {
 		baseValue = newBaseValue;
 	}
+}
+
+void Item::setName(const std::string & newName) {
+	name = newName;
+}
+
+void Item::setDescription(const std::string & newDescription) {
+	description = newDescription;
 }
--- Items/Item.hpp
+++ Items/Item.hpp
@@ -1,13 +1,23 @@
 #pragma once
+#include <string>
+
 class Item {
 private:
 	int weight = 0;
 	int baseValue = 0;
+	std::string name;
+	std::string description;
 public:
+	Item();
 
 	int getWeight();
 	int getBaseValue();
+	std::string getName();
+	std::string getDescription();
+
+
 	void setWeight(const int & newWeight);
 	void setBaseValue(const int & newBaseValue);
+	void setName(const std::string & newName);
+	void setDescription(const std::string & newDescription);
 };
-
