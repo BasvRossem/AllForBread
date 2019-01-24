33fe7f14c7ece48735388825b01c953d85538b18
Joepieler <be.campagne@live.nl>
2019-01-24 14:47:49 +0100

    add constuctor without std::function

--- Core/Tile.cpp
+++ Core/Tile.cpp
@@ -13,6 +13,16 @@ Tile::Tile(std::string & imageName, sf::Vector2f & position, sf::Vector2f & size
 	}
 }
 
+Tile::Tile(std::string & imageName, sf::Vector2f & position, sf::Vector2f & size)
+{
+	if (texture.loadFromFile(imageName)) {
+		sf::Vector2f sizeImage = sf::Vector2f{ texture.getSize() };
+		sprite.setTexture(texture);
+		sprite.setPosition(position);
+		sprite.setScale(sf::Vector2f{ size.x / sizeImage.x, size.y / sizeImage.y });
+	}
+}
+
 void Tile::activate() {
 	function();
 }
